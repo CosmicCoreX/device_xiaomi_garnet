5326cca7a50532f6eac1f5900d0f6a2a3c0154d8
Z E U S <iamdrzeus14@gmail.com>
2026-06-26 05:47:36 +0000

    garnet: Build lunaris dolby

--- device.mk
+++ device.mk
@@ -145,9 +145,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8450/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
-# Dolby
+# LunarisDolby
 PRODUCT_PACKAGES += \
-    DolbyAtmos
+    LunarisDolby
 
 # DRM
 PRODUCT_PACKAGES += \
